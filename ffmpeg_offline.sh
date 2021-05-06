226671a4829178b86e20cbed8fe9df874e423021
deltaway-grjj <75840201+deltaway-grjj@users.noreply.github.com>
2021-05-06 11:39:45 -0300

    Update ffmpeg_offline.sh

--- ffmpeg_offline.sh
+++ ffmpeg_offline.sh
@@ -3,7 +3,7 @@ wget ftp://teste:@192.168.10.238/packages.tar.gz
 tar -xzf packages.tar.gz
 sudo dpkg -i archives/*.deb
 #git clone https://github.com/bytedeco/javacpp-presets.git --branch 1.5.4
-cd javacpp-presets
+cd ffmpeg/javacpp-presets/
 xmlstarlet ed --inplace -d "/_:project/_:profiles/_:profile[_:id='javacpp-platform-linux-armhf']/_:modules/_:module" pom.xml
 xmlstarlet ed --inplace -s "/_:project/_:profiles/_:profile[_:id='javacpp-platform-linux-armhf']/_:modules" -t elem -n "module" -v "ffmpeg" pom.xml
 # JAVA_HOME="/usr/lib/jvm/java-11-openjdk-armhf/bin/java"
