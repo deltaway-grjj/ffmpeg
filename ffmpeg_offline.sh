a2e3446fb66f5f8814ddad5a176ba1046c458671
deltaway-grjj <75840201+deltaway-grjj@users.noreply.github.com>
2021-05-06 08:51:38 -0300

    Update ffmpeg_offline.sh

--- ffmpeg_offline.sh
+++ ffmpeg_offline.sh
@@ -1,65 +1,7 @@
 #!/bin/sh
-sudo apt update && sudo apt -y install \
-ant \
-autoconf \
-autoconf-archive \
-automake \
-build-essential \
-bzip2 \
-ccache \
-clang \
-cmake \
-file \
-freeglut3-dev \
-gfortran \
-git \
-git-core \
-gzip \
-libasound2-dev \
-libass-dev \
-libatlas-base-dev \
-libavcodec-dev \
-libavformat-dev \
-libcanberra-gtk* \
-libfreetype6-dev \
-libgnutls28-dev \
-libgtk-3-dev \
-libgtk2.0-dev \
-libjpeg-dev \
-libpng-dev \
-libprotobuf-dev \
-libsdl2-dev \
-libswscale-dev \
-libtiff-dev \
-libtool \
-libusb-dev \
-libv4l-dev \
-libva-dev \
-libvdpau-dev \
-libvorbis-dev \
-libx264-dev \
-libxcb1-dev \
-libxcb-shm0-dev \
-libxcb-xfixes0-dev \
-libxvidcore-dev \
-make \
-maven \
-nasm \
-#openjdk-8-jdk \
-patch \
-perl \
-pkg-config \
-protobuf-compiler \
-python \
-swig \
-tar \
-texinfo \
-unzip \
-wget \
-xmlstarlet \
-yasm \
-zlib1g \
-zlib1g-dev
+wget ftp://teste:@192.168.10.238/packages.tar.gz
+tar -xzf packages.tar.gz
+sudo dpkg -i archives/*.deb
 git clone https://github.com/bytedeco/javacpp-presets.git --branch 1.5.4
 cd javacpp-presets
 xmlstarlet ed --inplace -d "/_:project/_:profiles/_:profile[_:id='javacpp-platform-linux-armhf']/_:modules/_:module" pom.xml
